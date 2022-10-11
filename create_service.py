d1b9eac0f27c0e85074d14388ee515c82e49f5c6
JING Huihao <91329273+Egbertjing@users.noreply.github.com>
2022-10-11 17:17:33 +0800

    build:https

--- create_service.py
+++ create_service.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
         host="0.0.0.0",
         port=3609,
         reload=False,
-        forwarded_allow_ips ='*'
+        forwarded_allow_ips ='*',
+        ssl_keyfile="./furion.key",
+        ssl_certfile="./furion.pem"
     )
